98e3f5a40a5e1cca4d378b6d9cd60b536ddee6b1
GitHub Action <action@github.com>
2022-09-05 07:25:42 +0000

    Template cleanup

--- src/main/kotlin/com/github/chenmougit/groovynavigationplugin/services/MyApplicationService.kt
+++ src/main/kotlin/com/github/chenmougit/groovynavigationplugin/services/MyApplicationService.kt
@@ -1,6 +1,6 @@
-package org.jetbrains.plugins.template.services
+package com.github.chenmougit.groovynavigationplugin.services
 
-import org.jetbrains.plugins.template.MyBundle
+import com.github.chenmougit.groovynavigationplugin.MyBundle
 
 class MyApplicationService {
 
